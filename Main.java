5b48f07838268a1dfccaa283bb1936ca3d28ebb1
SrDakrFrame (João Victor Alves ) <jvalvessouza2005@gmail.com>
2025-09-26 15:52:32 -0300

    Janela daora

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
-// oipa
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+
+public static void main (String[] args) {
+    JFrame Janela = new JFrame ("Aps 4° Semestre");
+    Janela.setSize(500,400);
+    Janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Janela.add(new JLabel("Comentário da janela"));
+    Janela.setVisible(true);
+}}
