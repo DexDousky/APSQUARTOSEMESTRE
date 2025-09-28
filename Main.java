aa4d7c45d1f34e24395b9c1325dcab5a8fb04385
SrDakrFrame (João Victor Alves ) <jvalvessouza2005@gmail.com>
2025-09-28 16:17:02 -0300

    .

--- Main.java
+++ Main.java
@@ -6,6 +6,13 @@ public class Main {
 
 public static void main (String[] args) {
     JFrame Janela = new JFrame ("Aps 4° Semestre");
+    
+    // - CONFIGURAÇÕES DE JANELA -
+    
+    Janela.setSize(1270,800);
+    Janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Janela.add(new JLabel("Faz o L"));
+    Janela.setVisible(true);
 
     // - BOTÕES -
     
@@ -13,15 +20,8 @@ public static void main (String[] args) {
     botao_queimadas.setBounds(0,0,100,100);
     JButton botao = new JButton("Num sei");
     botao.setBounds(100,0,100,100);
-    
-    // - CONFIGURAÇÕES DE JANELA -
-    
+        
     Janela.add(botao_queimadas);
     Janela.add(botao);
-    Janela.setSize(500,400);
-    Janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Janela.add(new JLabel("Faz o L"));
-    Janela.setVisible(true);
-    
     }
 }
